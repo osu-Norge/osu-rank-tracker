aab4b6589d8e0630f74d8dc5a66cc55fe648029e
LBlend <24893890+LBlend@users.noreply.github.com>
2019-11-18 00:59:54 +0100

    added set_footer func to error templates

--- cogs/utils/Defaults.py
+++ cogs/utils/Defaults.py
@@ -5,7 +5,7 @@ async def error_fatal_send(ctx, text, *, mention=False):
     """Message template for fatal errors"""
 
     embed = discord.Embed(color=0xFF0000, description=f'❌ {text}')
-    embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
+    await set_footer(ctx, embed)
 
     if mention:
         return await ctx.send(content=ctx.author.mention, embed=embed)
@@ -17,7 +17,7 @@ async def error_warning_send(ctx, text, *, mention=False):
     """Message template for non-fatal errors"""
 
     embed = discord.Embed(color=0xF1C40F, description=f'⚠️ {text}')
-    embed.set_footer(icon_url=ctx.author.avatar_url, text=f'{ctx.author.name}#{ctx.author.discriminator}')
+    await set_footer(ctx, embed)
 
     if mention:
         return await ctx.send(content=ctx.author.mention, embed=embed)
