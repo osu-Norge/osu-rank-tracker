4a76bc6c254064e0c9c6dba5116f80ad2f5e0081
LBlend <lblend@protonmail.com>
2021-04-27 19:10:24 +0200

    method documentation

--- src/cogs/utils/database.py
+++ src/cogs/utils/database.py
@@ -22,11 +22,27 @@ class Guild(Database):
         self.id = id
 
     async def get_all(self) -> tuple:
+        """
+        Fetches all the info about a guild in the database
+
+        Returns
+        -----------
+        tuple: Database row
+        """
+
         self.cursor.execute('SELECT * FROM guild WHERE discord_id=%s', ([self.id]))
         return self.cursor.fetchone()
 
     @property
     async def prefix(self) -> str:
+        """
+        Fetches the guild's prefix
+
+        Returns
+        -----------
+        str: The guild's prefix
+        """
+
         self.cursor.execute('SELECT prefix FROM guild WHERE discord_id=%s', ([self.id]))
         response = self.cursor.fetchone()
         if response:
@@ -34,18 +50,46 @@ class Guild(Database):
 
     @property
     async def locale(self) -> str:
+        """
+        Fetches the guild's locale
+
+        Returns
+        -----------
+        str: The guild's locale
+        """
+
         self.cursor.execute('SELECT locale FROM guild WHERE discord_id=%s', ([self.id]))
         response = self.cursor.fetchone()
         if response:
             return response[0]
 
     async def is_oauth(self) -> bool:
+        """
+        Checks whether or not oAuth is enabled
+
+        Returns
+        -----------
+        bool
+        """
+
         self.cursor.execute('SELECT oauth FROM guild WHERE discord_id=%s', ([self.id]))
         response = self.cursor.fetchone()
         if response:
             return response[0]
 
     async def is_country_whitelisted(self, country_code: str) -> bool:
+        """
+        Checks whether or not a specified country code is in the list of whitelisted countries
+
+        Parameters
+        ----------
+        country_code (str): ISO 3166-1 Alpha-2 country code
+
+        Returns
+        -----------
+        bool
+        """
+
         self.cursor.execute('SELECT whitelisted_countries FROM guild WHERE discord_id=%s', ([self.id]))
         response = self.cursor.fetchone()
         if response and country_code in response:
