d601f392c7591efa5e33da03b56b1c918d4ecb96
LBlend <lblend@protonmail.com>
2021-05-07 00:47:01 +0200

    filter commands with checks

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -7,16 +7,35 @@ class Help(commands.Cog):
         self.bot = bot
         self.bot.remove_command('help')
 
-    @commands.command(hidden=True)
-    async def help(self, ctx, *command):
+    async def filter_command(self, ctx: commands.Context, command: commands.Command) -> bool:
+        """
+        Filters commands that fails check decorators. Also filters disabled commands
 
-        if not command:
+        Parameters
+        ----------
+        ctx (discord.ext.commands.Context): The Discord context
+        command (discord.ext.commands.Command): The Discord command object
+
+        Returns
+        ----------
+        bool: Whether or not the command is executable and viewable based on Discord context.
+        """
+
+        try:
+            return await command.can_run(ctx)
+        except commands.CommandError:
+            return False
+
+    @commands.command(hidden=True)
+    async def help(self, ctx, *commands):
+
+        if not commands:
             for cog in self.bot.cogs.values():
                 embed = discord.Embed(color=ctx.me.color, title=cog.qualified_name)
                 embed.set_author(name=ctx.me.name, icon_url=ctx.me.avatar_url)
 
                 for command in cog.get_commands():
-                    if not command.hidden or not command.enabled:
+                    if not command.hidden and await self.filter_command(ctx, command):
                         embed.add_field(name=command.name, value=command.help, inline=False)
 
                 if embed.fields:
