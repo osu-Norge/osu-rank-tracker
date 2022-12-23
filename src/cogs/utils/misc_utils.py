f175d067f8599dbce15fdad02defa3384107433f
LBlend <lblend@protonmail.com>
2022-12-23 03:37:39 +0100

    remove duplicate get_color from misc_utils

--- src/cogs/utils/misc_utils.py
+++ src/cogs/utils/misc_utils.py
@@ -1,7 +1,6 @@
 
 
 from contextlib import contextmanager
-import discord
 
 
 @contextmanager
@@ -17,23 +16,3 @@ def ignore_exception(*exceptions: Exception):
         yield
     except exceptions:
         pass
-
-
-def get_color(discord_object: discord.User | discord.Member | discord.Role) -> discord.Color:
-    """
-    Returns the user's top role color
-
-    Parameters
-    -----------
-    discord_object (discord.User|discord.Member|discord.Role): A discord object that has a color attribute
-
-    Returns
-    -----------
-    (discord.Color): The user's displayed color
-    """
-
-    if hasattr(discord_object, 'color') and str(discord_object.color) != '#000000':
-        return discord_object.color
-
-    return discord.Colour(0x99AAB5)
-
